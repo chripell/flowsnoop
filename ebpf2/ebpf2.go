e973d869b885ad4db8efb039e4e6f51ff627b666
Christian Pellegrin <chri@evolware.org>
2020-05-25 21:14:42 +0100

    Refactored for better reuse while reading IPv4 and IPv6 maps.

--- ebpf2/ebpf2.go
+++ ebpf2/ebpf2.go
@@ -19,15 +19,32 @@ import (
 	"gopkg.in/restruct.v1"
 )
 
+type connMap struct {
+	fd C.int
+	k  []byte
+	kp unsafe.Pointer
+	v  []byte
+	vp unsafe.Pointer
+}
+
 type Ebpf2 struct {
 	consumer flow.Consumer
 	obj      *C.struct_flowsnoop2
-	conn4FD  C.int
-	conn4K   []byte
-	conn4KP  unsafe.Pointer
-	conn4V   []byte
-	conn4VP  unsafe.Pointer
 	finished chan struct{}
+	conn4    *connMap
+	conn6    *connMap
+}
+
+func newConnMap(m *C.struct_bpf_map) *connMap {
+	connDef := C.bpf_map__def(m)
+	cm := &connMap{
+		fd: C.bpf_map__fd(m),
+		k:  make([]byte, connDef.key_size),
+		v:  make([]byte, connDef.value_size),
+	}
+	cm.kp = unsafe.Pointer(&cm.k[0])
+	cm.vp = unsafe.Pointer(&cm.v[0])
+	return cm
 }
 
 func (ebpf *Ebpf2) Init(consumer flow.Consumer) error {
@@ -39,6 +56,7 @@ func (ebpf *Ebpf2) Init(consumer flow.Consumer) error {
 	if ebpf.obj == nil {
 		return errors.New("failed to open eBPF object")
 	}
+	// TODO: make interface configurable
 	iface := "eth0\000"
 	for i, ch := range []byte(iface) {
 		ebpf.obj.rodata.targ_iface[i] = C.char(ch)
@@ -50,12 +68,8 @@ func (ebpf *Ebpf2) Init(consumer flow.Consumer) error {
 		return fmt.Errorf("failed to attach eBPF program: %d", ret)
 	}
 	ebpf.finished = make(chan struct{})
-	ebpf.conn4FD = C.bpf_map__fd(ebpf.obj.maps.connections)
-	conn4Def := C.bpf_map__def(ebpf.obj.maps.connections)
-	ebpf.conn4K = make([]byte, conn4Def.key_size)
-	ebpf.conn4KP = unsafe.Pointer(&ebpf.conn4K[0])
-	ebpf.conn4V = make([]byte, conn4Def.value_size)
-	ebpf.conn4VP = unsafe.Pointer(&ebpf.conn4V[0])
+	ebpf.conn4 = newConnMap(ebpf.obj.maps.connections)
+	ebpf.conn6 = newConnMap(ebpf.obj.maps.connections6)
 	// TODO: resize if entries different
 	return nil
 }
@@ -63,6 +77,25 @@ func (ebpf *Ebpf2) Init(consumer flow.Consumer) error {
 func memsetLoop(a []byte, v byte) {
 	for i := range a {
 		a[i] = v
+	}
+}
+
+func loopMap(m *connMap, fl interface{}, appEntry func(len uint64)) error {
+	memsetLoop(m.k, 0xff)
+	for {
+		if C.bpf_map_get_next_key(m.fd,
+			m.kp, m.kp) != 0 {
+			return nil
+		}
+		if ret := C.bpf_map_lookup_and_delete_elem(m.fd,
+			m.kp, m.vp); ret != 0 {
+			return fmt.Errorf("cannot find key: %d", ret)
+		}
+		if err := restruct.Unpack(m.k,
+			binary.BigEndian, fl); err != nil {
+			return fmt.Errorf("unpacking of flow failed: %v", err)
+		}
+		appEntry(binary.LittleEndian.Uint64(m.v))
 	}
 }
 
@@ -82,29 +115,29 @@ func (ebpf *Ebpf2) Run(ctx context.Context, flush <-chan (chan<- error)) {
 				break
 			}
 			// IPv4
-			memsetLoop(ebpf.conn4K, 0xff)
-			for {
-				if C.bpf_map_get_next_key(ebpf.conn4FD,
-					ebpf.conn4KP, ebpf.conn4KP) != 0 {
-					break
-				}
-				if ret := C.bpf_map_lookup_and_delete_elem(ebpf.conn4FD,
-					ebpf.conn4KP, ebpf.conn4VP); ret != 0 {
-					chErr <- fmt.Errorf("cannot find key: %d", ret)
-					return
-				}
-				var fl flow.Sample4
-				if err := restruct.Unpack(ebpf.conn4K,
-					binary.BigEndian, &fl); err != nil {
-					chErr <- fmt.Errorf("unpacking of flow failed: %v", err)
-					return
-				}
-				flows4 = append(flows4, flow.Sample4L{
-					Flow: fl,
-					Tot:  binary.LittleEndian.Uint64(ebpf.conn4V),
-				})
+			var fl4 flow.Sample4
+			if err := loopMap(ebpf.conn4, &fl4,
+				func(n uint64) {
+					flows4 = append(flows4, flow.Sample4L{
+						Flow: fl4,
+						Tot:  n,
+					})
+				}); err != nil {
+				chErr <- err
+				return
 			}
 			// IPv6
+			var fl6 flow.Sample6
+			if err := loopMap(ebpf.conn6, &fl6,
+				func(n uint64) {
+					flows6 = append(flows6, flow.Sample6L{
+						Flow: fl6,
+						Tot:  n,
+					})
+				}); err != nil {
+				chErr <- err
+				return
+			}
 			// Push to consumer
 			if err := ebpf.consumer.Push(time.Now(), flows4, nil, flows6, nil); err != nil {
 				chErr <- fmt.Errorf("error from consumer: %w\n", err)
